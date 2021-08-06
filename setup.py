c3de4a99640b4540a61bf658bb987076ff6375be
A-Lohse <augustlohse@yahoo.dk>
2021-08-06 11:36:57 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='academic_tweet',
-    version='0.1.0',    
+    version='0.3.0',    
     description='An API wrapper for academic track access to the v2 Twitter API',
     url='https://github.com/A-Lohse/academic_tweet',
-    download_url = "https://github.com/A-Lohse/academic_tweet/archive/refs/tags/v0.2.tar.gz",
+    download_url = "https://github.com/A-Lohse/academic_tweet/archive/refs/tags/v.0.3.tar.gz",
     author='August Lohse',
     author_email='al@augustlohse.com',
     license='MIT',
